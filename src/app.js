47bcecd035a309c963fee9676dbc2540571e6ea4
Romain <romainbigalion@hotmail.fr>
2024-02-12 22:06:09 +0100

    express 03

--- src/app.js
+++ src/app.js
@@ -7,11 +7,12 @@ const movieControllers = require("./controllers/movieControllers");
 app.get("/api/movies", movieControllers.getMovies);
 app.get("/api/movies/:id", movieControllers.getMovieById);
 app.post("/api/movies",movieControllers.postMovie)
-
+app.put("/api/movies/:id",movieControllers.putMovie)
 const userControllers = require("./controllers/usersControllers")
 
 app.get("/api/users", userControllers.getUsers);
 app.get("/api/users/:id", userControllers.getUserById);
 app.post("/api/users",userControllers.postUser)
+app.put("/api/users/:id",userControllers.putUser)
 
 module.exports = app;
